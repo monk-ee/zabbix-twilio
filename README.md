9e41ea081dbe92371036df2dc0195455fa68e53d
Lyndon Swan <magic.monkee.magic@gmail.com>
2013-12-16 14:42:57 +1000

    tested and ready

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 zabbix-twilio
 =============
+Just a little wrapper for the TwilioRestClient.
 
 Requirements
 ===
@@ -10,15 +11,20 @@ Requirements
 
 Installation
 ===
+Download the zip from github and place:
+
+    cp zabbix-twilio.py /etc/zabbix/alert.d/
+    cp config.yml.sample /etc/zabbix/alert.d/config.yml
 
 Configuration
 ===
+API Credentials
+The TwilioRestClient needs your Twilio credentials.
+
  + account_number:
  + account_token:
  + from_number
- API Credentials
 
-The TwilioRestClient needs your Twilio credentials.
 
 Usage
 ===
--- zabbix-twilio.py
+++ zabbix-twilio.py
@@ -1,21 +1,24 @@
-__author__ = 'monkee'
+#!/usr/bin/python
 
-#!/usr/bin/python2.7
+__author__ = 'monkee'
 from twilio.rest import TwilioRestClient
 import yaml, sys, getopt
 
 
 def main(argv):
+    phonenumber = ''
+    message = ''
+
     try:
-      opts, args = getopt.getopt(argv,"hi:o:",["phonenumber=","message="])
+      opts, args = getopt.getopt(argv,"h:n:m:",["phonenumber=","message="])
     except getopt.GetoptError:
-      print 'zabbix-twilio.py -n <phonenumber> -m <message>'
+      print 'Error: zabbix-twilio.py -n <phonenumber> -m <message>'
       sys.exit(2)
 
     for opt, arg in opts:
           if opt == '-h':
-             print 'zabbix-twilio.py -n <phonenumber> -m <message>'
-             sys.exit()
+             print 'Usage: zabbix-twilio.py -n <phonenumber> -m <message>'
+             sys.exit(2)
           elif opt in ("-n", "--phonenumber"):
              phonenumber = arg
           elif opt in ("-m", "--message"):
@@ -25,18 +28,19 @@ def main(argv):
     try:
         configStr = open('config.yml','r')
         config = yaml.load(configStr)
-    except BaseException, emsg:
-        print('Cannot find or parse config file: ' + emsg)
+    except (OSError, IOError), emsg:
+        print('Cannot find or parse config file: ' + str(emsg))
         sys.exit(2)
 
     try:
         client = TwilioRestClient(config['account_number'], config['account_token'])
-        client.messages.create(to=phonenumber, from_=config['from_number'],
+        cm = client.messages.create(to=phonenumber, from_=config['from_number'],
                                      body=message)
-        sys.exit()
     except BaseException, emsg:
-         print('Cannot send message: ' + emsg)
+         print('Cannot send message: ' + str(emsg))
          sys.exit(2)
+
+    sys.exit()
 
 if __name__ == "__main__":
    main(sys.argv[1:])
